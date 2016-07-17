bd070b7b43ba20ec80caa32c72e58ef91bcb615c
jcarolinares <jcarolinares@gmail.com>
2016-07-17 12:31:30 +0200

    Major improvements like a better quotes filter and a quotes print function with a random mode

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # wikiquote_webbot
-A little program that makes scraping from wikiquote
+A little program that makes scraping from wikiquote showing quotes from a search.
 
-WORK IN PROGRESS
+The program uses BeautifulSoup and Python 3. You can find more info of this amazing library in:
+
+https://www.crummy.com/software/BeautifulSoup/
+
+Made by Julián Caro Linares
+
+jcarolinares@gmail.com
+
+CC-BY-SA
--- wikiquote_webbot.py
+++ wikiquote_webbot.py
@@ -3,26 +3,45 @@
 from bs4 import BeautifulSoup
 import os
 import requests
+import random
 
-print("WIKIQUOTE WEBBOT\n")
+
+#Global variables
+language={"spanish":"es","english":"en","italian":"it","polish":"pl","turkish":"tr","french":"fr","russian":"ru","german":"de","bulgarian":"bg","chinese":"zh"}
+quotes=[]#The quotes container
+
+print("WIKIQUOTE WEBBOT\n\n")
 
 keywords=input("Please enter the keywords quote\n")
-language={"spanish":"es","english":"en","italian":"it","polish":"pl","turkish":"tr","french":"fr","russian":"ru","german":"de","bulgarian":"bg","chinese":"zh"}
+print("\n\nQUOTES\n")
 
+#Functions
+def print_quotes(number_of_quote):#Where -1 is all the quotes and -2 a random quote
+
+    if number_of_quote==-1:
+        for quote in quotes:
+            print(quote+"\n\n"+"Quote about: "+keywords+"\n\n")
+    elif number_of_quote==-2:
+        print(random.choice(quotes)+"\n\n"+"Quote about: "+keywords+"\n\n")
+    else:
+        print(quotes[number_of_quote]+"\n\n"+"Quote about: "+keywords+"\n\n")
+
+
+#Main script
 
 #Downloading the page
 r  = requests.get('http://'+language["spanish"]+'.wikiquote.org/wiki/'+keywords)
 data = r.text
 soup = BeautifulSoup(data,"lxml")
 
-#https://es.wikiquote.org/wiki/Madonna
+web_body=soup.find_all('div',class_="mw-content-ltr") #We take only the container of the quotes
 
-web_body=soup.find_all('div',class_="mw-body-content")
+list_of_quotes=web_body[0].find_all('li')
 
 #Quotes
-quotes=[]
-for actual_quote in web_body:
+#for actual_quote in web_body:
+for actual_quote in list_of_quotes:
 	quotes.append(actual_quote.get_text())
-	print(actual_quote.get_text())
+	#print(actual_quote.get_text())
 
-#<div id="bodyContent" class="mw-body-content">
+print_quotes(-1)
